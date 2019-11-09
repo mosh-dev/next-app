e2bd5841016c18f4174b27628d907c78f411d7f6
Moshiour <moshiour.rahman@ackosoft.com>
2019-11-09 23:20:24 +0600

    Npm update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,11 @@ import React, {Component} from 'react';
 import withDrawer from '../app/components/drawerWrapper';
 import withMaterialUI from '../app/components/shared/mui/with-mui';
 import {PostsComponent} from '../app/components/postsComponent';
-import {CommonDataService} from '../app/services/commonData.service';
 
 
 class Home extends Component {
   static async getInitialProps(): Promise<any> {
-    const cds = CommonDataService.getInstance();
-    let posts = await cds.getPosts();
-    posts = (posts as any[]).slice(0, 8);
-    posts
-      .forEach(post => post.thumb = 'https://picsum.photos/280/160/');
-    return {posts}
+    return {posts: []}
   }
 
   render() {
